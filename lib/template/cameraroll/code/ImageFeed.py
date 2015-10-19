703bbe9d05db57faf7eca340a4ed6a464614311f
arsread <arsread@gmail.com>
2015-10-19 23:41:09 +0200

    TODO:1.css on navbar;2.CV;3.new posts;4.iamge responsive

---
+++ lib/template/cameraroll/code/ImageFeed.py
@@ -0,0 +1,51 @@
+import time
+import copy
+import datetime
+
+
+IMG_ITEM = '''
+<a class="example-image-link" href="${PREFIX}images/__img__" data-lightbox="__date__" data-title="__year____place__"><img class="example-image" src="${PREFIX}thumbs/thumb___img__" alt="__place__" /></a>
+'''
+
+
+def get_feed(generator):
+    tformat = "%Y-%m-%d"
+
+    res = []
+    for md in generator.entry_cached:
+        entries = generator.entry_cached[md][0]
+
+        tmp_dict = {}
+        for entry in entries.values():
+            timestamp = time.mktime(datetime.
+                                    datetime.
+                                    strptime(entry.meta['post-time'], tformat).
+                                    timetuple())
+            if timestamp not in tmp_dict:
+                tmp_dict[timestamp] = []
+            tmp_dict[timestamp].append(entry)
+
+            meta = {}
+
+        for timestamp in tmp_dict:
+            entry_list = tmp_dict[timestamp]
+
+            meta['action'] = "New photos in " + generator.default['section']
+            meta['date'] = entry_list[0].meta['post-time']
+            places = []
+            for entry in entry_list:
+                places.append(entry.meta['place'])
+            places = set(places)
+            meta['title'] = ', '.join(places[:3]) + 'and so on'
+            meta['section'] = generator.default['section']
+            desc = []
+            for entry in entry_list[:4]:
+                img_item = copy.deepcopy(IMG_ITEM)
+                img_item = img_item.replace('__year__', entry.meta['year'])
+                img_item = img_item.replace('__img__', entry.meta['img'])
+                img_item = img_item.replace('__place__', entry.meta['place'])
+                desc.append(img_item)
+            meta['description'] = desc
+            meta['link'] = '${PREFIX}index.html'
+            res.append((timestamp, meta))
+    return res
